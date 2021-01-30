d7656187f25d1e49dbfe1d9a21af203e251c636a
desttruptor <41233120+desttruptor@users.noreply.github.com>
2021-01-30 22:01:45 +0300

    ui is ready

--- app/src/main/java/me/podlesnykh/tinkofftesttask/presentation/fragment_hot/FragmentHot.kt
+++ app/src/main/java/me/podlesnykh/tinkofftesttask/presentation/fragment_hot/FragmentHot.kt
@@ -1,21 +1,30 @@
 package me.podlesnykh.tinkofftesttask.presentation.fragment_hot
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import me.podlesnykh.tinkofftesttask.R
+import me.podlesnykh.tinkofftesttask.databinding.FragmentPostBinding
 
 class FragmentHot : Fragment() {
-
+    private var _binding: FragmentPostBinding? = null
+    private val binding get() = _binding!!
     private lateinit var viewModel: FragmentHotViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_post, container, false)
+    ): View {
+        _binding = FragmentPostBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
